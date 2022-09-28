3597df2ea9b1b071a3451f1e4240376b369065c2
kiranloka <112564858+kiranloka@users.noreply.github.com>
2022-09-27 20:59:20 -0700

    Update years.java

--- years.java
+++ years.java
@@ -1,8 +1,28 @@
-public class years
- {  
-   public static void main (String args[])  
-    {   int days, years;  
-                         days = 799;  
-                         years = days / 365;  
-                         System.out.println("Number of years is:"+years);  
-    }}  
+import java.util.Scanner;
+class days
+{
+ public static void main(String[] args)
+ {
+  try
+ {
+ Scanner sc=new Scanner(System.in);
+ 
+ System.out.print("Enter the days: ");
+ float days=sc.nextFloat();
+ if(days<0)
+ System.out.print("Enter the days correctly");
+ else
+ {
+ 
+ System.out.println("No. of years: "+(int)(days/365));
+ System.out.println("No. of weeks: "+(int)((days%365)/7));
+ System.out.println("No. of days: "+(int)((days%365)%7));
+ }
+}
+
+ catch(Exception e)
+  {
+    System.out.print("Enter the days correctly");
+   }
+}
+}
