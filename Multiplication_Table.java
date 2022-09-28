eec434268bac47031d18f15bfc96939352700591
kiranloka <112564858+kiranloka@users.noreply.github.com>
2022-09-28 01:06:08 -0700

    Update Multiplication_Table.java

--- Multiplication_Table.java
+++ Multiplication_Table.java
@@ -6,7 +6,9 @@ public class Multiplication_Table
         Scanner s = new Scanner(System.in);
 	System.out.print("Enter number:");        
 	int n=s.nextInt();
-        for(int i=1; i <= 10; i++)
+    System.out.print("Enter till which to multiply: ");
+    int m=s.nextInt();
+        for(int i=1; i <=m; i++)
         {
             System.out.println(n+" * "+i+" = "+n*i);
         }
