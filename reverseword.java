e6cd43f8012f242e0621a98c5bb2f8d62ffd74d9
kiranloka <112564858+kiranloka@users.noreply.github.com>
2022-11-01 10:52:44 +0530

    Update reverseword.java

--- reverseword.java
+++ reverseword.java
@@ -11,3 +11,24 @@ public class reverse {
  System.out.print(str.charAt(j-1));}
  }
 }
+if(up>low){
+ for(int i=low;i<=up;i++){
+ double sqrt=Math.sqrt(i);
+
+ if(sqrt-Math.floor(sqrt)==0&&fun(i)<10)
+ li.add(i);
+ }
+ }
+
+ System.out.println(li);
+ }
+public static int fun(int i){
+ int sum=0;
+ while(i!=0){
+ int rem=i%10;
+ sum=sum+rem;
+ i=i/10;
+ }
+ return sum;
+}
+}
